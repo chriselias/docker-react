d5b1210163cdbc95d43a9e20c3a3d89cb7456529
Chris Elias <chris@chriselias.com>
2020-03-10 18:22:12 -0500

    added some other text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed
+          Learn React!!!! wow...
         </a>
       </header>
     </div>
